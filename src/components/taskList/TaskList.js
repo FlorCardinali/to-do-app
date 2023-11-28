5b7cfc9523677ff5734e734269d8b70545aea86a
Florencia Cardinali <florenciapaulacardinali@gmail.com>
2023-11-28 03:33:09 -0300

    se agregaron ejemplos practicos a modo de data para testeo

--- src/components/taskList/TaskList.js
+++ src/components/taskList/TaskList.js
@@ -2,12 +2,56 @@ import React from "react"
 import TaskItem from "../taskItem/TaskItem";
 
 const TaskList = () => {
+
+    const data = [
+        {
+            title: "Lavar la ropa",
+            description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco"
+        },
+        {
+            title: "Darle de comer a los gatos",
+            description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco"
+        },
+        {
+            title: "Respirar",
+            description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco"
+        },
+        {
+            title: "Cantar una cancion de Miranda",
+            description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco"
+        },
+        {
+            title: "Estudiar para legislacion",
+            description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco"
+        },
+        {
+            title: "Comer con mama",
+            description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco"
+        },
+        {
+            title: "Ir a comprar al mayorista",
+            description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco"
+        },
+        {
+            title: "Hacer el proyecto de Argentina Programa",
+            description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco"
+        },
+        {
+            title: "Volver a respirar",
+            description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco"
+        },
+        {
+            title: "Volver a Estudiar",
+            description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco"
+        }
+    ]
+
     return (
-    <TaskItem 
-    title="Lorem ipsum dolor sit amet." 
-    // max 200 caracteres
-    description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-    "/> 
+    <>
+        {/* description max 200 caracteres */}
+        {data.map((i) => <TaskItem title={i.title} description={i.description}/>)}  
+    </>
+   
     );
 }
 export default TaskList;
