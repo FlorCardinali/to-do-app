9dc5d53186c55e3556cd1ca88acba8f64dc316d3
Florencia Cardinali <florenciapaulacardinali@gmail.com>
2023-11-28 03:17:53 -0300

    taskitems funcionando con delete y check, hovers y forma responsive

--- src/components/taskItem/TaskItem.js
+++ src/components/taskItem/TaskItem.js
@@ -1,13 +1,36 @@
 import React,{useState} from "react"
+import style from "../taskItem/TaskItem.module.css"
 
 const TaskItem = (props) => {
-    const [state, setState] = new useState(false);
+    const [state, setState] = useState(false);
+    const [visible, setVisible] = useState(true);
+    function handleClick(){
+        setState(!state);
+    }
+    function deleteTask(){
+        setVisible(false);
+
+        alert("Eliminado con exito!")
+    }
 
     return (
-    <div className={state ? "notDo" : "do"}>
-        <h2>{props.title}</h2>
-        <p>{props.description}</p>
+    <div style={{display: visible? "flex" : "none"}} className={state ? style.do : style.notDo}>
+        <section className={style.taskText}>
+            <h2>{props.title}</h2>
+            <p>{props.description}</p>
+        </section>
+       <span>
+        <button 
+        className={state ? style.normalButton : style.checkButton} 
+        onClick={() => handleClick()}>
+            {state ? "Uncheck" : "Check"}
+        </button>
+        <button className={style.deleteButton} onClick={() => deleteTask()}>Delete</button>
+       </span>
     </div>
     );
 }
+
+
+
 export default TaskItem;
--- src/components/taskList/TaskList.js
+++ src/components/taskList/TaskList.js
@@ -3,7 +3,11 @@ import TaskItem from "../taskItem/TaskItem";
 
 const TaskList = () => {
     return (
-    <TaskItem title="asd" description="asdasdasd"/> 
+    <TaskItem 
+    title="Lorem ipsum dolor sit amet." 
+    // max 200 caracteres
+    description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+    "/> 
     );
 }
 export default TaskList;
